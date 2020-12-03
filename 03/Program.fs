55dc48e82f003cf88f01d332df395e62c54ce42e
Ralph Hendriks <mail@ralphhendriks.nl>
2020-12-03 22:50:44 +0100

    Refactor day 3
    Simplify recursive inner function, using idiomatic function name `loop`.

--- 03/Program.fs
+++ 03/Program.fs
@@ -1,34 +1,34 @@
 open System.IO
 
-type Square = | OpenSpace | Tree
+type Square = Empty | Tree
 
 [<EntryPoint>]
 let main _ =
     let map = File.ReadAllLines("input.txt")
     
-    let find x y =
+    let elementAt x y =
         match y with
         | yy when yy < map.Length ->
             let row = map.[yy]
             match row.[x % row.Length] with
-            | '.' -> Some OpenSpace
+            | '.' -> Some Empty
             | '#' -> Some Tree
             | _ -> failwith "Unknown character"
         | _ -> None
     
     let countTrees (sx, sy) =
-        let rec path (x, y) =
-            match find x y with
-            | Some square ->
-                square :: path ((x + sx), (y + sy))
-            | None -> []
-        path (0, 0) |> List.filter (fun s -> s = Tree) |> List.length
+        let rec loop (x, y) =
+            match elementAt x y with
+            | None -> 0
+            | Some Tree -> 1 + loop ((x + sx), (y + sy))
+            | Some Empty -> loop ((x + sx), (y + sy))
+        loop (0, 0)
     
     printfn "Answer 1: %i" (countTrees (3, 1))  
 
     [| (1, 1); (3, 1); (5, 1); (7, 1); (1, 2) |]
     |> Seq.map (countTrees >> int64)
-    |> Seq.reduce (fun x y -> x * y)
+    |> Seq.reduce (*)
     |> printfn "Anwer 2: %i"
     
     0 // return an integer exit code
