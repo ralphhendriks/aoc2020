fcf5181241eb1c2d5dfa41bc9251c8b5cb14c411
Ralph Hendriks <mail@ralphhendriks.nl>
2020-12-06 08:52:12 +0100

    Refactor day 6

--- 06/Program.fs
+++ 06/Program.fs
@@ -3,23 +3,20 @@ open System.IO
 
 [<EntryPoint>]
 let main _ =
-    let groups = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine)
-    
+    let groups =
+        File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine)
+        |> Array.map (fun group ->
+            group.Split(Environment.NewLine)
+            |> Seq.map Set.ofSeq)
+
     groups
-    |> Array.map (fun group ->
-        group.Replace(Environment.NewLine, "")
-        |> Set.ofSeq
-        |> Set.count)
+    |> Array.map (Set.unionMany >> Set.count)
     |> Array.sum
     |> printfn "Answer 1: %i"
-    
+
     groups
-    |> Array.map (fun group ->
-        group.Split(Environment.NewLine)
-        |> Seq.map Set.ofSeq
-        |> Set.intersectMany
-        |> Set.count)
+    |> Array.map (Set.intersectMany >> Set.count)
     |> Array.sum
-    |> printfn "Answer 2: %A"
-        
+    |> printfn "Answer 2: %i"
+
     0 // return an integer exit code
